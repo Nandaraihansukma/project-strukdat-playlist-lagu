3935c3778c08818df16a21f77016fd1e967a783b
bagus2705 <bagusprasetyo2705@gmail.com>
2021-05-04 22:10:55 +0700

    interface

--- interface.cpp
+++ interface.cpp
@@ -1,91 +1,8 @@
 #include <iostream>
 
-struct lagu{
-std::string judul;
-std::string penyanyi;
-int rank;
-lagu *next;
-lagu *prev;
-lagu *left;
-lagu *right;
-};
-
-using pointer=lagu*;
-using Tree=pointer;
-
-struct Queue {
- pointer Head;
- pointer Tail;
-};
-
-Queue createQueue(){
-  Queue baris;
-  baris.Head=nullptr;
-  baris.Tail=nullptr;
-  return baris;
-}
-
-Tree createTree(){
-    return nullptr;
-}
-void searchTree(){
-
-}
-void searchStack(){
-
-}
-void replaceStack(){
-
-}
-void replaceTree(){
-
-}
-void dequeue(){
-
-}
-void playing( Queue &Top){
-std::cout<<Top.Head->judul;
-}
-void infoLagu(){
-
-}
-void createNode(pointer& newNode){
-newNode =new lagu;
-std::cout<<"Masukkan Judul lagu :";
-std::cin>>newNode->judul;
-std::cout<<"Masukkan Penyanyi lagu :";
-std::cin>>newNode->penyanyi;
-std::cout<<"Masukkan Ranking lagu :";
-std::cin >>newNode->rank;
-newNode->next=nullptr;
-newNode->prev=nullptr;
-newNode->left=nullptr;
-newNode->right=nullptr;
-}
-void enqueue(Queue &q,pointer newNode){
-
-}
-void insertTree(Tree& root, pointer newNode){
-if(root == nullptr){
-    root = newNode;
-    }else{
-  bool isLowerThanRoot = (newNode->rank < root->rank);
-
-  if(isLowerThanRoot){
-    insertTree(root->left, newNode);}
-  else{
-    insertTree(root->right, newNode);
-}
-}
-}
-
 int main(){
-    int menu,exit=0;
-    pointer newNode;
-  auto Baris=createQueue();
-  auto Root=createTree();
-    do{
-    std::cout<<"-----------------------"<<"\n";
+    int menu,exit=0;;
+    do{std::cout<<"-----------------------"<<"\n";
     std::cout<<"-----------------------"<<"\n";
     std::cout<<"Program Playlist Musik"<<"\n";
     std::cout<<"Menu"<<"\n";
@@ -101,28 +18,31 @@ int main(){
     std::cin>>menu;
     switch(menu){
         case(1):
-        createNode(newNode);
-        insertTree(Root,newNode);
-        enqueue(Baris,newNode);
+        std::cin>>
         break;
 
         case(2):
+        std::cin>>
         break;
 
         case(3):
-        
+        std::cin>>
         break;
 
         case(4):
+        std::cin>>
         break;
 
         case(5):
+        std::cin>>
         break;
 
         case(6):
+        std::cin>>
         break;
 
         case(7):
+        std::cin>>
         break;
         case(8):
         std::cout<<"-----------------------"<<"\n";
@@ -143,4 +63,5 @@ int main(){
             break;
     }
     }while(exit==0);
+}
 }
